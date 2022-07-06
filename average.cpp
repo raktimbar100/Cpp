18eacf5c13478ea5d9a6b39ab0257ab8b82de3d3
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-07 03:11:19 +0530

    Create average.cpp

---
+++ average.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int p,c,m;
+ cout<<"Enter physics & chemistry & mathematics marks respectively:"<<endl;
+ cin>>p;
+ cin>>c;
+ cin>>m;
+float average=(p+c+m)/3.0;
+cout<<"Average is "<<average<<endl;
+return 0;	
+}
