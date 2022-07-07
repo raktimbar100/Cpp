4ced11843d44774ccf349b5b0f7915037176d9e9
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-07 19:11:45 +0530

    Create pyramid.cpp

---
+++ pyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the value of n:"<<endl;
+	cin>>n;
+	int i,j;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			cout<<" ";
+		}
+		for(j=1;j<=2*i-1;j++)
+		{
+			cout<<"*";
+		}
+		cout<<"\n";
+		
+		
+		
+	}
+}
