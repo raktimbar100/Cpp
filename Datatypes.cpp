407acab2211975b767f7ae889453c35f0d9afd24
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-07 12:46:21 +0530

    Create Datatypes.cpp

---
+++ Datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	bool x=5>3;
+	bool y=false;
+	int t=67;
+	float z=8.9868648;
+	double c=8.9868648;
+	char letter='a';
+	char dollar='$';
+	cout<<x<<endl;
+	cout<<y<<endl;
+	cout<<t<<endl;
+	cout<<setprecision(10)<<z<<endl;
+	cout<<setprecision(10)<<c<<endl;
+	cout<<letter<<endl;
+	cout<<dollar<<endl;
+}
