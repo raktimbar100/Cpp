5da9e4a27a228d56ab1f0d96dcd2e71adf043fae
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-07 12:50:48 +0530

    Create Sizeof_datatypes.cpp

---
+++ Sizeof_datatypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	bool x=5>3;
+	bool y=false;
+	int t=67;
+	float z=8.9868648;
+	double c=8.9868648;
+	char letter='a';
+	char dollar='$';
+	
+	cout<<sizeof(y)<<endl;//boolean
+	cout<<sizeof(int)<<endl;//integer
+	cout<<sizeof(float)<<endl;//float
+	cout<<sizeof(double)<<endl;//double
+	cout<<sizeof(char)<<endl;//character
+	
+}
