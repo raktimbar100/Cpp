6dafb5bd417d3631238900c168eb6d4eb56d14dc
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-07 17:13:07 +0530

    Create bitwiseoperatorex.cpp

---
+++ bitwiseoperatorex.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int a=5;
+	int b=7;
+	int res=a&b;
+	int res1=a|b;
+	int res2=a^b;
+	int res3=~a;
+	int res4=a<<2;
+	int res5=a>>2;
+	cout<<res<<endl;
+		cout<<res1<<endl;
+			cout<<res2<<endl;
+				cout<<res3<<endl;
+					cout<<res4<<endl;
+						cout<<res5<<endl;
+}
