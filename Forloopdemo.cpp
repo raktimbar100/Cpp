ede51dab90f57ea5669cada771346b259db3c440
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-07 18:38:18 +0530

    Create Forloopdemo.cpp

---
+++ Forloopdemo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the value of n"<<endl;
+	cin>>n;
+	int i;
+	int sum=0;
+	for(i=1;i<=n;i++)
+	{
+		sum+=i;
+	}
+	cout<<"sum is "<<sum;
+}
